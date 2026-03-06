8609a943bb3265c8ef34b35b8649f12748a7ac75
Gabriel <delducadev@gmail.com>
2026-03-06 13:35:45 -0300

    chore: setup inicial do projeto spring boot com dependencias h2 e jpa
    Closes #4

---
+++ core-backend/src/test/java/com/example/core_backend/CoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
