beb7113b74ccca33a26309d4a54575756e5ca85b
Gabriel <delducadev@gmail.com>
2026-03-07 13:17:57 -0300

    chore: configuracao do cliente axios, setup inicial do client-ui com vite e limpeza dos arquivos de template
    Closes #7

---
+++ client-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Smart Feedback Hub</h1>
+      {/* Aqui vão entrar os componentes em breve */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client-ui/src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080/api/feedbacks',
+});
+
+export default api;
+
+/*
+Axios foi escolhido ao invés do fetch porque permite criar uma 'instância' pré-configurada, facilitando a reutilização e a manutenção do código. 
+Importei a lib, criei uma instância com a URL base da API e exportei essa instância para ser usada em outros arquivos.
+*/
